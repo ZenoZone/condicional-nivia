21af9cdccb4d5959d427bc8e476a5b7632cb16f1
ZenoZone <enzo.silva76@etec.sp.gov.br>
2024-05-21 10:09:28 -0300

    trabalho nivia 4

---
+++ Form3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaNivia_Condicional
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+            double idade;
+            string sexo;
+
+
+            
+            idade = Convert.ToDouble(txtidade.Text);
+            sexo = Convert.ToString(txtSexo.Text);
+
+            if(sexo != "Masculino")
+            {
+
+            }
+        }
+
+        private void btmVoltar_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaNivia_Condicional
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtb1.Clear();
+            txtb2.Clear();
+            txtb1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double i, b1,b2;
+            
+
+            b1 = Convert.ToDouble(txtb1.Text);
+            i = b1 - 1;
+            b2 = b1 * i;
+            txtb2.Text = string.Concat(txtb2.Text, "\r\n", b1, "x", i, "=", b2.ToString());
+            i--;
+
+            while (i > 0)
+            {
+
+               
+                b1 = b2;
+                b2 = b2 * i;
+                txtb2.Text = string.Concat(txtb2.Text, "\r\n", b1, "x", i, "=", b2.ToString());
+                i--;
+
+
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double i, b1, b2;
+
+
+            b1 = Convert.ToDouble(txtb1.Text);
+            i = b1 - 1;
+            b2 = b1 * i;
+            txtb2.Text = string.Concat(txtb2.Text, "\r\n", b1, "x", i, "=", b2.ToString());
+            i--;
+
+            do
+            {
+
+
+                b1 = b2;
+                b2 = b2 * i;
+                txtb2.Text = string.Concat(txtb2.Text, "\r\n", b1, "x", i, "=", b2.ToString());
+                i--;
+
+
+
+            } while (i > 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double i, b1, b2;
+
+
+            b1 = Convert.ToDouble(txtb1.Text);
+            i = b1 - 1;
+            b2 = b1 * i;
+            txtb2.Text = string.Concat(txtb2.Text, "\r\n", b1, "x", i, "=", b2.ToString());
+
+            for(i = b1-2; i > 0; i--)
+            {
+                b1 = b2;
+                b2 = b2 * i;
+                txtb2.Text = string.Concat(txtb2.Text, "\r\n", b1, "x", i, "=", b2.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
